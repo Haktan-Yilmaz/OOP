792680756880fb34dbba4660c88721c7a3e8ab0c
Haktan-Yilmaz <9021333@student.zadkine.nl>
2024-05-28 13:16:08 +0200

    Opdracht 02

--- oefenopdracht/opdracht 01/Product.php
+++ oefenopdracht/opdracht 01/Product.php
@@ -2,17 +2,23 @@
 
 class Product
 {
-
+    public $name = "Een game";
 }
 
-$chair1 = new Product ();
-$chair2 = new Product ();
-$chair3 = new Product ();
-$chair4 = new Product ();
-$chair5 = new Product ();
+$game1 = new Product();
+$game1->name = "People Playground";
 
-var_dump($chair1);
-var_dump($chair2);
-var_dump($chair3);
-var_dump($chair4);
-var_dump($chair5);
+$game2 = new Product();
+$game2->name = "Dying Light 2";
+
+$game3 = new Product();
+
+echo $game1->name."<br>";
+echo $game2->name."<br>";
+echo $game3->name."<br>";
+
+$game1->name = "Ark Survival Evolved";
+echo $game1->name."<br>";
+
+var_dump($game1);
+var_dump($game2);
